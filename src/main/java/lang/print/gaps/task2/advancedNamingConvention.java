1b139a8e21931e1a7dcf01e2e4e4075381d2bf12
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-19 00:21:28 +0600

    Update advancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -6,7 +6,7 @@ public class advancedNamingConvention {
     private int age;
     private int phoneNumber;
 
-    public void CAllToFriend() {
+    public void callToFriend() {
         //some code
     }
 
